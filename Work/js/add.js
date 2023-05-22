dfd799280297e14fa5a29d6dd1a2d1d32bef1b8b
fderrf3234 <gogetassj4ul4@gmail.com>
2023-05-22 11:19:54 +0300

    update add js

--- Work/js/add.js
+++ Work/js/add.js
@@ -1,141 +1,176 @@
-var questionType = document.getElementById("question");
-var likertScale = document.getElementById("likert_scale");
-var yesNo = document.getElementById("yes_no");
-var shortAnswer = document.getElementById("short_answer");
-var mcq = document.getElementById("mcq");
-likertScale.style.display = "none";
-yesNo.style.display = "none";
-shortAnswer.style.display = "none";
-mcq.style.display = "none";
+const questionContainer = document.getElementById("question_container");
+const addQuestionBtn = document.getElementById("addquestion");
+const submitBtn = document.getElementById("submit");
 
-
-questionType.addEventListener("change", function () {
-  if (questionType.value == "likert") {
-    likertScale.style.display = "block";
-    yesNo.style.display = "none";
-    shortAnswer.style.display = "none";
-    mcq.style.display = "none";
-  } else if (questionType.value == "yesno") {
-    likertScale.style.display = "none";
-    yesNo.style.display = "block";
-    shortAnswer.style.display = "none";
-    mcq.style.display = "none";
-  } else if (questionType.value == "shortanswer") {
-    likertScale.style.display = "none";
-    yesNo.style.display = "none";
-    shortAnswer.style.display = "block";
-    mcq.style.display = "none";
-  } else if (questionType.value == "mcq") {
-    likertScale.style.display = "none";
-    yesNo.style.display = "none";
-    shortAnswer.style.display = "none";
-    mcq.style.display = "block";
+function createOptionContainer(questionType, index) {
+  const optionContainer = document.createElement("div");
+  optionContainer.classList.add("option-container");
+  switch (questionType) {
+    case "likert":
+      optionContainer.innerHTML = `
+        <label>Likert Scale Options:</label>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="likert${index}" id="option1" value="1">
+          <label class="form-check-label" for="option1">Strongly Disagree</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="likert${index}" id="option2" value="2">
+          <label class="form-check-label" for="option2">Disagree</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="likert${index}" id="option3" value="3">
+          <label class="form-check-label" for="option3">Neutral</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="likert${index}" id="option4" value="4">
+          <label class="form-check-label" for="option4">Agree</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="likert${index}" id="option5" value="5">
+          <label class="form-check-label" for="option5">Strongly Agree</label>
+        </div>`;
+      break;
+    case "yesno":
+      optionContainer.innerHTML = `
+        <label>Yes or No Options:</label>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="yesno${index}" id="yes" value="or">
+          <label class="form-check-label" for="yes">Yes</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="yesno${index}" id="no" value="no">
+          <label class="form-check-label" for="no">No</label>
+        </div>`;
+      break;
+    case "shortanswer":
+      optionContainer.innerHTML = `
+        <label for="short_answer_text">Short Answer:</label>
+        <input type="text" id="short_answer_text" name="short_answer_text" class="form-control">
+      `;
+      break;
+    case "mcq":
+      optionContainer.innerHTML = `
+        <label>MCQ Options:</label>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="mcq${index}" id="mcq_option1" value="option1">
+          <input type="text" class="form-control" placeholder="Option 1" name="mcq_option1_text">
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="mcq${index}" id="mcq_option2" value="option2">
+          <input type="text" class="form-control" placeholder="Option 2" name="mcq_option2_text">
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="mcq${index}" id="mcq_option3" value="option3">
+          <input type="text" class="form-control" placeholder="Option 3" name="mcq_option3_text">
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="mcq${index}" id="mcq_option4" value="option3">
+          <input type="text" class="form-control" placeholder="Option 4" name="mcq_option4_text">
+        </div>`;
+      break;
   }
-});
-// Get the add question button element
-var addQuestionBtn = document.getElementById("addquestion");
+  return optionContainer;
+}
 
-// Add click event listener to the add question button
-addQuestionBtn.addEventListener("click", function () {
-  // Create a new form element
-  const newForm = document.createElement("form");
-  
-  // Set innerHTML for the new form element
-  newForm.innerHTML = `
-  <label for="question"class="question-label">Question Type:</label>
-  <select id="question" name="question">
-    <option value="likert">Likert Scale</option>
-    <option value="yesno">Yes or No</option>
-    <option value="shortanswer">Short Answer</option>
-    <option value="mcq">Multiple Choice</option>
-  </select><br><br>
+function updateOptionContainer(selectElement) {
+  const questionType = selectElement.value;
+  const optionContainer = selectElement.parentElement.nextElementSibling;
+  const index =
+    Array.from(
+      selectElement.parentElement.parentElement.parentElement.children
+    ).indexOf(selectElement.parentElement.parentElement) + 1;
+  const newOptionContainer = createOptionContainer(questionType, index);
+  optionContainer.replaceWith(newOptionContainer);
+}
 
-  <label for="question_text"class="question-label">Question Text:</label>
-  <input type="text" id="question_text" name="question_text"><br><br>
+function addQuestion() {
+  const questionCount = questionContainer.childElementCount + 1;
+  const question = document.createElement("div");
+  question.classList.add("question");
+  question.innerHTML = `
+    <h3>Question ${questionCount}</h3>
+    <div class="form-group">
+      <label for="question_text">Question Text:</label>
+      <textarea id="question_text" name="question_text" rows="3" class="form-control"></textarea>
+    </div>
+    <div class="form-group">
+      <label for="question_type_select">Question Type:</label>
+      <select class="form-control" id="question_type_select_${questionCount}">
+        <option value="likert">Likert Scale</option>
+        <option value="yesno">Yes or No</option>
+        <option value="shortanswer">Short Answer</option>
+        <option value="mcq">Multiple Choice</option>
+      </select>
+    </div>
+  `;
+  const initialOptionContainer = createOptionContainer(
+    "likert",
+    questionCount
+  );
+  question.appendChild(initialOptionContainer);
 
-  <div id="likert_scale">
-    <label for="likert_scale_text"class="question-label">Likert Scale Options:</label>
+  // Add event listener to update option container when question type is changed
+  const selectElement = question.querySelector(
+    `#question_type_select_${questionCount}`
+  );
+  selectElement.addEventListener("change", () =>
+    updateOptionContainer(selectElement)
+  );
 
-    <label for="option1">1</label>
-    <input type="radio" id="option1" name="likert" value="1">
-    <label for="option2">2</label>
-    <input type="radio" id="option2" name="likert" value="2">
-    <label for="option3">3</label>
-    <input type="radio" id="option3" name="likert" value="3">
-    <label for="option4">4</label>
-    <input type="radio" id="option4" name="likert" value="4">
-    <label for="option5">5</label>
-    <input type="radio" id="option5" name="likert" value="5">
-  </div>
+  // Add a remove question button
+  const removeQuestionBtn = document.createElement("button");
+  removeQuestionBtn.classList.add("btn", "btn-danger", "remove-question");
+  removeQuestionBtn.innerHTML = "Remove Question";
+  removeQuestionBtn.addEventListener("click", () =>
+    removeQuestion(question)
+  );
+  question.appendChild(removeQuestionBtn);
 
-  <div id="yes_no">
-    <label for="yes_no_text"class="question-label">Yes or No Options:</label><br>
-    <input type="radio" id="yes_no_text" name="yes_no_text" value="yes">Yes<br>
-    <input type="radio" id="yes_no_text" name="yes_no_text" value="no">No<br><br>
-  </div>
+  questionContainer.appendChild(question);
+  submitBtn.style.display = "block";
+}
 
-  <div id="short_answer">
-    <label for="short_answer_text">Short Answer:</label>
-    <input type="text" id="short_answer_text" name="short_answer_text"><br><br>
-  </div>
+function renumberQuestions() {
+  const questions = questionContainer.querySelectorAll(".question");
+  questions.forEach((question, index) => {
+    const header = question.querySelector("h3");
+    header.textContent = `Question ${index + 1}`;
+  });
+}
 
-  <div id="mcq">
-    <label for="option1_radio">Option 1:</label>
-    <input type="radio" id="option1_radio" name="mcq" value="option1">
-    <input type="text" id="option1" name="mcq_text" value=""><br>
-  
-    <label for="option2_radio">Option 2:</label>
-    <input type="radio" id="option2_radio" name="mcq" value="option2">
-    <input type="text" id="option2" name="mcq_text" value=""><br>
-  
-    <label for="option3_radio">Option 3:</label>
-    <input type="radio" id="option3_radio" name="mcq" value="option3">
-    <input type="text" id="option3" name="mcq_text" value=""><br>
-  
-    <label for="option4_radio">Option 4:</label>
-    <input type="radio" id="option4_radio" name="mcq" value="option4">
-    <input type="text" id="option4" name="mcq_text" value=""><br>
-`;
+function removeQuestion(question) {
+  questionContainer.removeChild(question);
+  renumberQuestions();
+  if (questionContainer.childElementCount === 0) {
+    submitBtn.style.display = "none";
+  }
+}
 
-  // Append the new form element to the DOM
-  document.getElementById("quiz-content").appendChild(newForm);
+// Event listeners
+addQuestionBtn.addEventListener("click", addQuestion);
 
-  // Set the default display style for the added form
-  var questionType = newForm.querySelector("#question");
-  var likertScale = newForm.querySelector("#likert_scale");
-  var yesNo = newForm.querySelector("#yes_no");
-  var shortAnswer = newForm.querySelector("#short_answer");
-  var mcq = newForm.querySelector("#mcq");
-  likertScale.style.display = "none";
-  yesNo.style.display = "none";
-  shortAnswer.style.display = "none";
-  mcq.style.display = "none";
+// Renumber questions when a question is removed
+function renumberQuestions() {
+  const questions = questionContainer.querySelectorAll(".question");
+  questions.forEach((question, index) => {
+    const header = question.querySelector("h3");
+    header.textContent = `Question ${index + 1}`;
+  });
+}
 
-  // Add event listener to question type select element to show/hide corresponding options
-  questionType.addEventListener("change", function () {
-    var selectedOption = questionType.value;
-    // Show/hide options based on selected question type
-    if (selectedOption === "likert") {
-      likertScale.style.display = "block";
-      yesNo.style.display = "none";
-      shortAnswer.style.display = "none";
-      mcq.style.display = "none";
-    } else if (selectedOption === "yesno") {
-      likertScale.style.display = "none";
-      yesNo.style.display = "block";
-      shortAnswer.style.display = "none";
-      mcq.style.display = "none";
-    } else if (selectedOption === "shortanswer") {
-      likertScale.style.display = "none";
-      yesNo.style.display = "none";
-      shortAnswer.style.display = "block";
-      mcq.style.display = "none";
-    } else if (selectedOption === "mcq") {
-      likertScale.style.display = "none";
-      yesNo.style.display = "none";
-      shortAnswer.style.display = "none";
-      mcq.style.display = "block";
-    }
-  });
-});
+// Initialize event listeners for existing questions
+function initializeQuestionListeners() {
+  const questions = questionContainer.querySelectorAll(".question");
+  questions.forEach((question, index) => {
+    const selectElement = question.querySelector("select.form-control");
+    selectElement.addEventListener("change", () =>
+      updateOptionContainer(selectElement)
+    );
+    const removeQuestionBtn = question.querySelector(".remove-question");
+    removeQuestionBtn.addEventListener("click", () =>
+      removeQuestion(question)
+    );
+  });
+}
 
+initializeQuestionListeners();
