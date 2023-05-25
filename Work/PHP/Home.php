8f269aa7dc693ae4d6247978297feeb0dc9bd17c
fderrf3234 <gogetassj4ul4@gmail.com>
2023-05-25 14:07:21 +0300

    added new interface

--- Work/PHP/Home.php
+++ Work/PHP/Home.php
@@ -1,9 +1,5 @@
-<?php
-
-session_start();
-if (isset($_SESSION['email']) && isset($_SESSION['password']) && isset($_SESSION['id'])) {
-
-    ?>
+<?php session_start();
+if (isset($_SESSION['email']) && isset($_SESSION['password']) && isset($_SESSION['id'])) { ?>
     <!DOCTYPE html>
     <html lang="en">
 
@@ -12,29 +8,37 @@ if (isset($_SESSION['email']) && isset($_SESSION['password']) && isset($_SESSION
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>My Website Home</title>
+        <link rel="stylesheet" href="../../Addition/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="../../Addition/css/all.min.css" />
+        <link rel="stylesheet" href="../Css/styles.css">
     </head>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> <a class="navbar-brand" href="Home.php">Homepage</a> <button
+            class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+            aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li><a class="nav-link" href="userProfile.php">Edit Profile</a></li>
+                <li class="nav-item"> <a class="nav-link" href="Logout.php">Logout</a> </li>
+            </ul>
+        </div>
+    </nav>
 
     <body>
-        <h1>Welcome to my website
-            <?php
-            if (empty($_SESSION['username'])) {
-                echo '<br><p>Email: '.$_SESSION['email'].'</p>';
-            }else {
-                echo '<p>'.$_SESSION['username'].'</p>';
-            }
-            ?>
-        </h1>
-        <li><a href="userProfile.php">User Profile</a></li>
-        <li><a href="displayQuizzes.php">Attempt quiz</a></li>
-        <li><a href="displayResponses.php">Edit Responses</a></li>
-        <li><a href="Logout.php">Logout</a></li>
-        
+        <h3>Welcome to Quizzy
+            <?php if (empty($_SESSION['username'])) {
+                echo '<br><p>Email: ' . $_SESSION['email'] . '</p>';
+            } else {
+                echo '<p>' . $_SESSION['username'] . '</p>';
+            } ?>
+            </h1> <br>
+             <li><a href="displayQuizzes.php">Attempt quiz</a></li> 
+            <li><a href="displayResponses.php">Edit Responses</a></li>
+            <script src="../../Addition/js/bootstrap.bundle.min.js"></script>
+            <script src="../../Addition/js/all.min.js"></script>
     </body>
 
     </html>
-
-    <?php
-} else {
+<?php } else {
     header("Location: Login.php");
     exit();
-}
+} ?>
